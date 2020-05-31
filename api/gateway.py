03c215a18e1691737295ae04e2c2e780bcc45c34
luoxin <luoxin.ttt@gmail.com>
2020-05-31 23:07:51 +0800

    fix

--- api/gateway.py
+++ api/gateway.py
@@ -1,6 +1,7 @@
 import traceback
 
 import requests
+import werkzeug
 from flask import request, abort, Blueprint, make_response, ctx
 from werkzeug.exceptions import NotFound as HttpNotFound
 
@@ -31,22 +32,25 @@ def gateway(path):
         host = request.host
         path = request.full_path
         method = request.method
-        headers = request.headers
+        req_headers = {h[0]: h[1] for h in request.headers}
         data = request.data or request.form or None
 
         # 自定义dns
-        host = g.dns_cache.get(host, host)
+        # host = g.dns_cache.get(host, host)
+        host = "203.195.187.254:3000"
 
         # 去掉gateway
         path = path.replace("/gateway/", "/", 1)
 
         # TODO 对header做一些特殊操作
 
+        print("{}://{}{}".format(scheme, host, path))
+
         try:
             with requests.request(
                 method,
                 "{}://{}{}".format(scheme, host, path),
-                headers=headers,
+                headers=req_headers,
                 data=data,
                 stream=True,
                 timeout=5,
@@ -54,10 +58,13 @@ def gateway(path):
                 if r.status_code != 200:
                     create_error(-1)
 
+                rsp_headers = {h[0]: h[1] for h in r.headers}
+
                 rsp = make_response(r.content)
                 # TODO 对返回的header做特殊的操作
                 # rsp.headers["X-Req-Id"] = utils.gen_uuid()
 
+                rsp.headers = werkzeug.datastructures.Headers(rsp_headers)
         except:
             logger.error("err:{}".format(traceback.format_exc()))
             create_error(-1)
